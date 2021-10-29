8fd60482ba1efee7f95a0f3e045b3bc6156f2c54
Ahmed Hany <ahmedhanyhosny@outlook.com>
2021-10-29 22:38:33 +0200

    solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, repeats) {
+    let repeatedString = '';
+    
+    for (let repeat = 0; repeat < repeats; repeat++) {
+        repeatedString += str;
+    }
+    
+    return (repeats >= 0) ? repeatedString : 'ERROR';
 };
 
 // Do not edit below this line
