8036550d9597b540ada535904cf6f53de3a65b7f
Ahmed Hany <ahmedhanyhosny@outlook.com>
2021-11-07 20:05:22 +0200

    solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let strLength = str.length;
+    
+    let strOriginal = "";
+    for (let c = 0; c < strLength; c++) {
+        if (str[c] >= "A" && str[c] <= "Z" || str[c] >= "a" && str[c] <= "z") {
+            strOriginal += str[c].toLowerCase();
+        } else {
+            continue;
+        }
+    }
 
+    let strReversed = "";
+    for (let c = strLength - 1; c >= 0; c--) {
+        if (str[c] >= "A" && str[c] <= "Z" || str[c] >= "a" && str[c] <= "z") {
+            strReversed += str[c].toLowerCase();
+        } else {
+            continue;
+        }
+    }
+    return strOriginal === strReversed;
 };
 
 // Do not edit below this line
