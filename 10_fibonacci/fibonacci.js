1dba8bf0e181355daafb3ff6356e315e9299fd97
Ahmed Hany <ahmedhanyhosny@outlook.com>
2021-11-07 20:12:21 +0200

    solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
 
+    if (member < 0) {
+        return "OOPS";
+    }
+
+    let a = 1, b = 1;
+    for (let i = 0; i < member - 2; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
 };
 
 // Do not edit below this line
